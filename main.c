d3c16c5b218a35a875880fb6ae2a264e8b45cbfa
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:01:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,34 +4,40 @@
 
 int main()
 {
-           int i,j,a,n;
+    int i, j, a, n;
 
     printf("n i girin ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int matris[n][n];
     int yer[n][n];
-    a=1;
+    a = 1;
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            matris[i][j]=a++;
-            printf("%3d",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            matris[i][j] = a++;
+            printf("%3d", matris[i][j]);
         }
         printf("\n");
     }
 
     printf("\n\n");
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            yer[j][i]=matris[i][j];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            yer[j][i] = matris[i][j];
         }
     }
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%3d",yer[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%3d", yer[i][j]);
         }
         printf("\n");
     }
